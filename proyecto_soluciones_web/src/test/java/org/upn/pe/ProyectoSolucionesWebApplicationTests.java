039e9666a723f0aeeb875cab4482a549a5f07fad
JEAN <jeancarlospoma@gmail.com>
2023-06-21 23:30:01 -0500

    creacion de proyecto

---
+++ proyecto_soluciones_web/src/test/java/org/upn/pe/ProyectoSolucionesWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.upn.pe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoSolucionesWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
